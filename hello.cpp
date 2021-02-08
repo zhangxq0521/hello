1bf1a841f0effe14602742c96f9edc74437a2675
zhangxq0521 <zhangxq0521@sohu.com>
2021-02-08 14:02:16 +0800

    Beautify source code.

--- hello.cpp
+++ hello.cpp
@@ -15,10 +15,15 @@ void hello() {
 
 
     Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
+    
+	Poco::DigestOutputStream ds(md5);
     ds << "abcdefghijklmnopqrstuvwxyz";
     ds.close();
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+	
+	
+	std::string strMD5 = Poco::DigestEngine::digestToHex(md5.digest());	
+    std::cout << "MD5 of string `abcdefghijklmnopqrstuvwxyz`" << strMD5 << std::endl;
+	
     return;
 
 }
